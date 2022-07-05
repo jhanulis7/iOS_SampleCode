581d045bcad33fee1561e8e2789d3208af25ac22
김재희 <jaehee.kim@hyundai.com>
2022-07-05 18:47:52 +0900

    playground 추가

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var name = "jaehee.kim"
+var age = 30
+
+func sayHello(name: String, age: Int) -> String {
+    return "my Nmme is \(name), age: \(age)"
+}
+
+
+sayHello(name:name, age:age)
