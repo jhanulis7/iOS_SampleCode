a1b2a102fcf91ce7e8fc3d687289caa339334662
김재희 <jaehee.kim@hyundai.com>
2022-07-06 09:57:40 +0900

    autolayout 실습

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by 7012618 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
